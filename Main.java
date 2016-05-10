70056e715b9850859bee9cf8fb01adb394cbd89a
Diogo <Diogo@DiogoStation>
2016-05-10 15:32:55 +0100

    Criação de metodo para uma criar uma linha, exemplos no main, metodo para gerir linhas, metodo para criar paragens

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package edu.ufp.inf.lp2.projecto;
-
+import java.util.ArrayList;
 /**
  *
  * @author bluis
@@ -16,6 +16,34 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Linha objlinha;
+        
+        objlinha = criarLinha("linhad");
+        
+        
+    }
+    
+    public static Linha criarLinha(String nome){
+       
+        Linha novalinha = new Linha(nome); // é criado um novo objecto do tipo Linha
+        return novalinha;     
+    }
+    public static ArrayList gerirLinhas(Linha objlinha)
+    {
+        ArrayList<Linha> listaLinhas = new ArrayList<>();
+        listaLinhas.add(objlinha);
+        return listaLinhas;
+    }
+    public static Paragem criarParagem(Linha objlinha, String nome, ArrayList listaLinhas, Coordenada novacoordenada, String cod, String zona)
+    {
+        Paragem novaparagem;
+        novaparagem = objlinha.addParagem(nome);
+        
+        novaparagem.setCod(nome);
+        novaparagem.setZona(zona);
+        novaparagem.setLinhas(listaLinhas);
+        novaparagem.setCoordenada(novacoordenada);
+        return novaparagem;
     }
     
 }
