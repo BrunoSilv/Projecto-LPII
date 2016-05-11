90e9639f761c9c6caa255d23ef4b415e862834df
Diogo <Diogo@DiogoStation>
2016-05-11 18:34:51 +0100

    Extendi a classe de forma a reutilizar codigo em vez de termos código em duas classes para manter arrays o que temos é uma classe com o codigo referente ao array e que podemos usar em cada objecto da classe paragem e linhas, alterei o metodo set linhas para adicionar as linhas a um array dessa forma conseguimos saber ao inserir se a linha existe ou nao, alterei o metodo addparagem compara se a paragem existe na bd define o campo da linha como sendo o da linha em que a estamos a inserir e retorna a paragem

--- Linha.java
+++ Linha.java
@@ -1,11 +1,11 @@
 package edu.ufp.inf.lp2.projecto;
 
+import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 
-public class Linha {
+public class Linha extends ListaParagens{
 
   public String nome;
-
   
   public Linha(String nome){
     this.nome = nome;          // Este constructor define o nome da linha
@@ -14,12 +14,12 @@ public class Linha {
    * 
    * @element-type Aresta
    */
-  public ArrayList  linhas;
+  public ArrayList<String> linhas = new ArrayList<>();
     /**
    * 
    * @element-type Paragem
    */
-  public ArrayList tem;
+  
     /**
    * 
    * @element-type Aresta
@@ -27,14 +27,38 @@ public class Linha {
   public ArrayList  myAresta;
 
   //definimos o nome e depois é criada a paragem
-  public Paragem addParagem(String nome) {
-  Paragem novaparagem = new Paragem();
-  novaparagem.setNomeParagem(nome);
-  return novaparagem;
+  public Paragem addParagem(String nomedaparagem, Linha novalinha, Paragem novaparagem) {
+      Paragem variavelaux = null;
+      boolean terminou = false;
+      
+      for(int i=0; i<novaparagem.getParagens().size() && !terminou; i++){
+       
+       variavelaux = (Paragem) novaparagem.getParagens().get(i);
+       if(variavelaux.getNomeParagem().equals(nomedaparagem)){  // vai procurar no array de paragens
+          variavelaux.setLinha(novalinha.getNome());            // se existe uma paragem com aquele nome
+       }else{                                                   // e define o campo linha como sendo o da linha em que vamos adicionar a paragem
+         StdOut.println("A paragem nao existe na BD");
+          terminou = true;
+          break;
+       }
+    }
+    return variavelaux; 
   }
 
   public Paragem removeParagem(String nome) {
   return null;
   }
+  public ArrayList getLinhas()
+  {
+     return linhas;
+  }
+  public void setLinhas(String nome){
+      linhas.add(nome);
+   }
+  public String getNome()
+  {
+      return nome;
+  }
+ 
   
 }
