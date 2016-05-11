9b9d1089031b5531ee05bebdbfd159dbd7a0a174
Diogo <Diogo@DiogoStation>
2016-05-11 18:39:32 +0100

    exemplo de codigo para nova paragem, adicionar a linha, adicionar paragem, imprimir paragem

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package edu.ufp.inf.lp2.projecto;
+import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 /**
  *
@@ -16,34 +17,54 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Linha objlinha;
+        Linha novalinha;
+        Paragem novaparagem;
         
-        objlinha = criarLinha("linhad");
         
-        
+        novaparagem = criarParagem();
+        novaparagem.adicionaraLista(novaparagem);
+        novalinha = criarLinha("linhad");
+        novaparagem = novalinha.addParagem("Trindade", novalinha, novaparagem);
+        StdOut.print(novaparagem.getLinha() + "\n" + novaparagem.getNomeParagem()+ "\n"); // a funcionar
+         
     }
     
-    public static Linha criarLinha(String nome){
-       
-        Linha novalinha = new Linha(nome); // é criado um novo objecto do tipo Linha
-        return novalinha;     
-    }
-    public static ArrayList gerirLinhas(Linha objlinha)
+    public static Linha criarLinha(String nome)
     {
-        ArrayList<Linha> listaLinhas = new ArrayList<>();
-        listaLinhas.add(objlinha);
-        return listaLinhas;
+       Linha novalinha = new Linha(nome);// é criado um novo objecto do tipo Linha
+        if(validarLinha(nome, novalinha) == true){
+            StdOut.print("Linha ja existe");
+        }else
+        {
+          novalinha.setLinhas(nome);
+        }
+        return novalinha;
     }
-    public static Paragem criarParagem(Linha objlinha, String nome, ArrayList listaLinhas, Coordenada novacoordenada, String cod, String zona)
+    
+    public static boolean validarLinha(String nome, Linha novalinha)
     {
-        Paragem novaparagem;
-        novaparagem = objlinha.addParagem(nome);
+        boolean acabou = false;
         
-        novaparagem.setCod(nome);
-        novaparagem.setZona(zona);
-        novaparagem.setLinhas(listaLinhas);
-        novaparagem.setCoordenada(novacoordenada);
+        for(int i =0; i<novalinha.getLinhas().size() && !acabou; i++)
+           if(novalinha.getLinhas().get(i).equals(nome))
+           {
+               acabou = true;
+               break;
+           }
+        return acabou;
+    }
+ 
+    public static Paragem criarParagem()
+    {
+        
+        String nome, cod, zona;
+        Coordenada novacoordenada = new Coordenada(45.84f,65.67f);
+        nome = "Trindade";
+        
+        Paragem novaparagem = new Paragem("Trindade", "C2",novacoordenada,"45",null);// o campo la linha é
+        //null porque nao sabemos qual é a linha que queremos adicionar a paragem
         return novaparagem;
+       
     }
     
 }
