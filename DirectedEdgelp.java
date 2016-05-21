ea1ad41c4bffbafaa6f7e3e9214c068270b097ef
Diogo <Diogo@DiogoStation>
2016-05-21 02:48:41 +0100

    acrescentei os atributos custoTemporal, custoMonetario, distancia e no construtor os this

---
+++ DirectedEdgelp.java
@@ -0,0 +1,124 @@
+/******************************************************************************
+ *  Compilation:  javac DirectedEdge.java
+ *  Execution:    java DirectedEdge
+ *  Dependencies: StdOut.java
+ *
+ *  Immutable weighted directed edge.
+ *
+ ******************************************************************************/
+
+package edu.ufp.inf.lp2.projecto;
+
+import edu.princeton.cs.algs4.*;
+
+/**
+ *  The <tt>DirectedEdge</tt> class represents a weighted edge in an 
+ *  {@link EdgeWeightedDigraph}. Each edge consists of two integers
+ *  (naming the two vertices) and a real-value weight. The data type
+ *  provides methods for accessing the two endpoints of the directed edge and
+ *  the weight.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/44sp">Section 4.4</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+
+public class DirectedEdgelp { 
+    private  int v;
+    private  int w;
+    private  double custoTemporal;
+    private float custoMonetario;
+    private double distancia;
+  
+    
+
+    /**
+     * Initializes a directed edge from vertex <tt>v</tt> to vertex <tt>w</tt> with
+     * the given <tt>weight</tt>.
+     * @param v the tail vertex
+     * @param w the head vertex
+     * @param distancia
+     * @param weight the weight of the directed edge
+     * @param custoMonetario
+     * @param custoTemporal
+     * @throws IndexOutOfBoundsException if either <tt>v</tt> or <tt>w</tt>
+     *    is a negative integer
+     * @throws IllegalArgumentException if <tt>weight</tt> is <tt>NaN</tt>
+     */
+    public DirectedEdgelp(int v, int w, double distancia, double custoTemporal, float custoMonetario) {
+        if (v < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(custoTemporal)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.distancia = distancia;
+        this.custoTemporal = custoTemporal;
+        this.custoMonetario = custoMonetario;  
+    }
+    
+    /**
+     * Returns the tail vertex of the directed edge.
+     * @return the tail vertex of the directed edge
+     */
+    public int from() {
+        return v;
+    }
+
+    /**
+     * Returns the head vertex of the directed edge.
+     * @return the head vertex of the directed edge
+     */
+    public int to() {
+        return w;
+    }
+
+    /**
+     * Returns the weight of the directed edge.
+     * @return the weight of the directed edge
+     */
+    public double weight() {
+        return custoTemporal;
+    }
+
+    /**
+     * Returns a string representation of the directed edge.
+     * @return a string representation of the directed edge
+     */
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", custoTemporal);
+    }
+
+    /**
+     * Unit tests the <tt>DirectedEdge</tt> data type.
+     */
+    public static void main(String[] args) {
+       // DirectedEdgelp e = new DirectedEdgelp(12, 34, 5.67);
+        //StdOut.println(e);
+    }
+}
+
+/******************************************************************************
+ *  Copyright 2002-2015, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
